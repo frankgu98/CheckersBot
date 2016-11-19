179581f6d87dddbe2f25cdd28c12d921ec81aa04
itsdreezy <agglingtonfartworthy7@yahoo.com>
2016-11-18 23:35:50 -0500

    Changed syntax for rotateToCoord
    added some more comments,

--- movexy and execute turn.c
+++ movexy and execute turn.c
@@ -5,8 +5,8 @@ rotatetocoord
 rotate2tocoord
 void moveXY(point p,actuator motorX,actuator motorY1,actuator motorY2)
 {
-	rotateToCoord(p.x, motorX);
-	rotateToCoord(p.y motorY1, motorY2);
+	rotateToCoord(motorX, p.x) ;
+	rotate2ToCoord(motorY1, motorY2, p.y);
 	
 }
 
@@ -15,38 +15,38 @@ void moveXY(point p,actuator motorX,actuator motorY1,actuator motorY2)
 //;et cupdown be the distance for the cup to lower
 //firat element of pickedmove is the the desired piece
 	
-void executeTurn(turn pickedMove[], actuator motorX, actuator motorY1,actuator motorY2,
+void executeTurn(turn pickedTurn], actuator motorX, actuator motorY1,actuator motorY2,
 				actuator motorZ)
 {
 	//moves arm to piece using moveXY
 	
-	moveXY(pickedMove[0], motorX, motorY1, motorY2);
+	moveXY(pickedTurn[0], motorX, motorY1, motorY2);
 	
 	//lowers the arm
-	rotateToCoord(cupdown, motorZ)
+	rotateToCoord(motorZ, cupdown)
 
 	//checks the size of the array
 	int SIZE = 0;
 	int n = 0;
 	
-	while (pickedMove[n] != sentinel)
+	while (pickedTurn] != sentinel)
 		{
 			SIZE++;
 			n++;
 		}	
 	//moves piece by iterating through picked move
+	//starts at the first DESTINATION (bc pickedTurn[0] is the desired piece)
 	
 	for (n=1; n < SIZE, n++)
-			moveXY(pickedMove[n], motorX, motorY1, motorY2);
+			moveXY(pickedTurn[n], motorX, motorY1, motorY2);
 
 	//check for crowning
 	
-
 	//indicate to player that piece has been crowned?
 
-	//stop move
-
 	//raises arm
+	
+	//back to idle
 
 
 }
