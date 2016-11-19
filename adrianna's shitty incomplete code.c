a9b1d12d29501ad83caed0fbd93f01c0656df603
itsdreezy <agglingtonfartworthy7@yahoo.com>
2016-11-18 21:47:14 -0500

    Add files via upload

---
+++ adrianna's shitty incomplete code.c
@@ -0,0 +1,41 @@
+
+void moveSteps(step step ,actuator motorX, actuator motorY)
+{
+	//
+
+
+
+
+
+
+}
+
+void executeMove(turn newTurn, int board[][8])
+{
+	//moves arm to piece using rotateToDist
+
+	//lowers the arm
+
+	//checks if move type is capture
+
+	//moves piece
+
+	//check for crowning
+
+	//indicate to player that piece has been crowned?
+
+	//stop move
+
+	//raises arm
+
+
+}
+
+
+
+task main()
+{
+
+
+
+}
