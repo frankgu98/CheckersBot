51edfdc51e66eb5b20e8609d3d0b986cf2769afd
itsdreezy <agglingtonfartworthy7@yahoo.com>
2016-11-20 15:14:00 -0500

    Fixed problems with arrays in functions

--- xy test.c
+++ xy test.c
@@ -1,11 +1,22 @@
 struct point{
 	int x,y;
 };
+
+void initPoint(point & p, int x, int y)
+{
+	p.x = x;
+	p.y = y;
+}
+
 struct actuator{
 	int motorI, powerLevel; //which motor (A,B,C,D), power to run motor at with + being away from initial origin at top, left, robot-side
 													//of board
 	float gearRadius, curCoord, maxCoord; //standard units are cm but it'll work as long as all the units are the same
 };
+
+//constant to convert coordinates to cm for rotate to dist
+const double tileDim = 3.5;
+
 
 //can't return struct....
 //i wish we had classes
@@ -93,74 +104,84 @@ bool rotate2ToCoord(actuator & act1, actuator & act2, float coord){
 }
 
 
-void moveXY(point p ,actuator motorX,actuator motorY1,actuator motorY2)
+void moveXY(point p0,actuator motorX,actuator motorY1,actuator motorY2)
 {
-	rotateToCoord(motorX, p.x) ;
-	rotate2ToCoord(motorY1, motorY2, p.y);
+	rotateToCoord(motorX, p0.x*tileDim) ;
+	rotate2ToCoord(motorY1, motorY2, p0.y*tileDim);
 
 }
-void executeTurn(point* pickedTurn, actuator motorX, actuator motorY1,actuator motorY2,
-				actuator motorZ)
+void executeTurn(point *pickedTurn,actuator motorX, actuator motorY1,actuator motorY2,
+				actuator motorZ, point sentinel, point idle)
 {
-	point sentinel;
-	sentinel.x=9;
-	sentinel.y=9;
+
 	//moves arm to piece using moveXY
 
-	moveXY(pickedTurn[0], motorX, motorY1, motorY2);
+	//i hate robotc
+	point pTemp;
+	pTemp.x = pickedTurn[0].x;
+	pTemp.y = pickedTurn[0].y;
+	moveXY(pTemp, motorX, motorY1, motorY2);
 
 	//lowers the arm
-	rotateToCoord(motorZ, 3);
+	rotateToCoord(motorZ, 5);
 
 	//checks the size of the array
 	int SIZE = 0;
 	int n = 0;
 
+	//change this when getmove returns an int indicating the size of the array
 	while (pickedTurn[n] != sentinel)
 		{
 			SIZE++;
 			n++;
 		}
 	//moves piece by iterating through picked move
-	//starts at the first DESTINATION (bc pickedTurn[0] is the desired piece)
+	//starts at the first DESTINATION (b/c pickedTurn[0] is the desired piece)
 
 	for (n=1; n < SIZE; n++)
-			moveXY(pickedTurn[n], motorX, motorY1, motorY2);
-
+	{
+			point tempP;
+			//robotC is gaaaarbaage
+			tempP.x = pickedTurn[n].x ;
+			tempP.y = pickedTurn[n].y ;
+			moveXY(tempP, motorX, motorY1, motorY2);
+	}
 	//check for crowning
 
 	//indicate to player that piece has been crowned?
 
 	//raises arm
+	rotateToCoord(motorZ, -5);
 
 	//back to idle
-
+	moveXY(idle, motorX, motorY1, motorY2);
 
 }
 task main()
 {
-	actuator motorX, motorY1, motorY2;
+	actuator motorX, motorY1, motorY2, motorZ;
 	initActuator(motorY1, motorA, 50, 2.1, 1000);
 	initActuator(motorY2, motorB, 50, 2.1, 1000);
 	initActuator(motorX, motorC, 50, 2.1, 1000);
-	point p0, p1,p2, p3;
+	initActuator(motorZ, motorD, 50, 2.1, 1000);
+	point idle, p0,p1,p2, p3;
+	point pickedTurn[5];
 
-	p0.x = 0;
-	p0.y = 0;
+	initPoint(idle, 0,0);
+	initPoint(p0,4,4);
+	initPoint(p1,1,1);
+	initPoint(p2,2,8);
+	initPoint(p3,3,3);
 
-	p1.x = 3.5;
-	p1.y = 3.5;
+	point sentinel;
+	sentinel.x=100;
+	sentinel.y=100;
 
-	p2.x = 7;
-	p2.y = 28;
+	pickedTurn[0] = p0;
+	pickedTurn[1] = p1;
+	pickedTurn[2] = p2;
+	pickedTurn[3] = p3;
+	pickedTurn[4] = sentinel;
 
-	p3.x = 10.5;
-	p3.y = 10.5;
-	moveXY(p0, motorX, motorY1, motorY2);
-	wait1Msec(1000);
-	moveXY(p1, motorX, motorY1, motorY2);
-	wait1Msec(1000);
-	moveXY(p2, motorX, motorY1, motorY2);
-	wait1Msec(1000);
-	moveXY(p3, motorX, motorY1, motorY2);
+	executeTurn(pickedTurn, motorX, motorY1, motorY2, motorZ, sentinel, idle);
 }
